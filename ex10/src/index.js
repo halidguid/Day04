d38c29b9a14e78893b17c5504aa096bed5765126
halidguid <halidhosi@gmail.com>
2021-03-12 01:32:45 +0100

    day04

---
+++ ex10/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+    // Only change code below this line
+
+    var myHomeTown = "Sarajevo";
+    var myfirstName = "Halid";
+    var myLastName = "Hosi";
+
+    myfirstName;
+    myLastName;
+    myHomeTown;
+    // Only change code above this line
+    return { myfirstName, myLastName, myHomeTown };
+
+}
+
+
+
+
+
+
+console.log(main());
+module.exports = main;
